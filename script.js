79d224f225ad51feb9e898e887aef7585872a324
selikyle <selkyle30@yahoo.com>
2026-03-26 11:07:20 -0400

    Selected elements using doucment.querySelector

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('JavaScript connected!');
+const heroHeading = document.querySelector(".hero");
+const nav = document.querySelector("nav");
+const projectSection = document.querySelector(".projects");
+
+console.log(heroHeading);
+console.log(nav);
+console.log(projectSection);
